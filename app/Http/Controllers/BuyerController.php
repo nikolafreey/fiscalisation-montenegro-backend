b9d157ccd0935f73a088b914c4a4bf7bc2f91549
Andrej Jokic <aj.jokic@gmail.com>
2020-11-23 20:56:20 +0100

    Project setup

---
+++ app/Http/Controllers/BuyerController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Buyer;
+use Illuminate\Http\Request;
+
+class BuyerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Buyer::all();
+    }
+
+    
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $buyer = Buyer::create($request->all());
+        return response()->json([$buyer], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Buyer  $buyer
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Buyer $buyer)
+    {
+        return response()->json($buyer, 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Buyer  $buyer
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Buyer $buyer)
+    {
+        $buyer->update($request->all());
+        return response()->json([$buyer], 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Buyer  $buyer
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Buyer $buyer)
+    {
+        $buyer->delete();
+        return response()->json('Deleted', 200);
+    }
+}
