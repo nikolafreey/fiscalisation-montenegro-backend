4a6a6884e4ae89c3faa72868d8c50e26249a3430
Željko Janjic <zeljko.janjic@payspotns.local>
2020-11-24 22:42:49 +0100

    RCN-53 API Rute

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,20 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\BuyerController;
+use App\Http\Controllers\DjelatnostController;
+use App\Http\Controllers\ModulController;
+use App\Http\Controllers\OvlascenoLiceController;
+use App\Http\Controllers\PartnerController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\PreduzeceController;
+use App\Http\Controllers\ZiroRacunController;
+use App\Http\Controllers\KategorijaController;
+use App\Http\Controllers\TipKorisnikaController;
+use App\Http\Controllers\FizickoLiceController;
+
+
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -20,7 +33,17 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::middleware('auth:sanctum')->group(function() {
-    Route::apiResource('/buyers', BuyerController::class);
+    Route::apiResource('/djelatnosti', DjelatnostController::class);
+    Route::apiResource('/moduli', ModulController::class);
+    Route::apiResource('/ovlascenalica', OvlascenoLiceController::class);
+    Route::apiResource('/tipovikorisnika', TipKorisnikaController::class);
+    Route::apiResource('/kategorije', KategorijaController::class);
+    Route::apiResource('/fizickalica', FizickoLiceController::class);
+    Route::apiResource('/preduzeca', PreduzeceController::class);
+    Route::apiResource('/ziroracuni', ZiroRacunController::class);
+    Route::apiResource('/partneri', PartnerController::class);
+
+
 });
 
 
