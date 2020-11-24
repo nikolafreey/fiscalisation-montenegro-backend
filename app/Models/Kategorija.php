7efcb05c23661544b0732c29ed9c93c21a018ed9
Nikola Vukovic <nikolav54@gmail.com>
2020-11-24 21:21:31 +0100

    RCN-53 dodate veze na kategorije i ovlascena lica

--- app/Models/Kategorija.php
+++ app/Models/Kategorija.php
@@ -3,10 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use PhpParser\Node\Expr\FuncCall;
 
 class Kategorija extends Model
 {
     protected $table = "kategorije";
 
     protected $fillable = array('naziv');
+
+    public function preduzeca()
+    {
+        return $this->hasMany('App\Models\Preduzece');
+    }
 }
--- app/Models/OvlascenoLice.php
+++ app/Models/OvlascenoLice.php
@@ -9,4 +9,9 @@ class OvlascenoLice extends Model
     protected $table = "ovlascena_lica";
 
     protected $fillable = array('ime', 'prezime', 'telefon', 'telefon_viber', 'telefon_whatsapp', 'telefon_facetime', 'email');
+
+    public function preduzeca()
+    {
+        return $this->belongsToMany('App\Models\Preduzece', 'ovlasceno_lice_preduzece', 'ovlasceno_lice_id', 'preduzece_id');
+    }
 }
