1cdd5371d38f3ab9387beb4d5d32f4e94c528e71
Nikola Vukovic <nikolav54@gmail.com>
2021-03-02 20:40:10 +0100

    Dodat global scope na DepozitWithdraw model, na Poslovnu Jedinicu nije potrebno

--- app/Models/DepozitWithdraw.php
+++ app/Models/DepozitWithdraw.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Scopes\UserScope;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -9,4 +10,8 @@ class DepozitWithdraw extends Model
 {
     use HasFactory;
 
+    protected static function booted()
+    {
+        static::addGlobalScope(new UserScope);
+    }
 }
