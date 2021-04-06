4423bef9d622ff41833318d07d1899e7c6290d2a
Aleksandar Milivojsa <milivojsa96@gmail.com>
2021-04-06 21:24:14 +0200

    Automatski odabir preduzeca ako korisnik ima samo jedno

--- app/Http/Middleware/OdabranoPreduzeceMiddleware.php
+++ app/Http/Middleware/OdabranoPreduzeceMiddleware.php
@@ -4,7 +4,6 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 
 class OdabranoPreduzeceMiddleware
@@ -12,8 +11,8 @@ class OdabranoPreduzeceMiddleware
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure $next
      * @return mixed
      */
     public function handle(Request $request, Closure $next)
@@ -22,12 +21,32 @@ class OdabranoPreduzeceMiddleware
             return $next($request);
         }
 
-        if (
-            DB::table('personal_access_tokens')
-                ->where('token', getAccessToken($request))
-                ->first()
-                ->preduzece_id  === null
-        ) {
+        $odabranoPreduzeceID = DB::table('personal_access_tokens')
+            ->where('token', getAccessToken($request))
+            ->first()
+            ->preduzece_id;
+
+        if ($odabranoPreduzeceID === null) {
+            $brojPreduzeca = auth()->user()->preduzeca()->count();
+
+            if ($brojPreduzeca === 1) {
+                $preduzece = auth()->user()->preduzeca->first();
+
+                $loggedInUsersIntoPreduzeceCount = DB::table('personal_access_tokens')
+                    ->where('preduzece_id', $preduzece->id)
+                    ->count();
+
+                if ($loggedInUsersIntoPreduzeceCount >= $preduzece->brojUredjaja) {
+                    return response()->json(['message' => 'Previse uredjaja je ulogovano na ovo preduzece']);
+                }
+
+                DB::table('personal_access_tokens')
+                    ->where('token', getAccessToken($request))
+                    ->update(['preduzece_id' => $preduzece->id]);
+
+                return $next($request);
+            }
+
             return response()->json('Nije odabrano preduzece', 401);
         }
 
