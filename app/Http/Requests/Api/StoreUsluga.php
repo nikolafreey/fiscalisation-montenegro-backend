418029253f872987b0b37cf35ff24b8b06ffe552
Nikola Vukovic <nikolav54@gmail.com>
2021-04-16 20:21:30 +0200

    Opis kod usluge nije obavezno polje

--- app/Http/Requests/Api/StoreUsluga.php
+++ app/Http/Requests/Api/StoreUsluga.php
@@ -15,7 +15,7 @@ class StoreUsluga extends FormRequest
     {
         return [
             'naziv' => 'required|string|max:50|min:3',
-            'opis' => 'string|max:255|min:3',
+            // 'opis' => 'string|max:255',
             'cijena_bez_pdv' => array('regex:/^-?[0-9]+(?:\.[0-9]{1,4})?$/', 'min:1', 'max:15'),
             'ukupna_cijena' => array('regex:/^-?[0-9]+(?:\.[0-9]{1,4})?$/', 'min:1', 'max:15'),
             'cijena_bez_pdv_popust' => array('regex:/^-?[0-9]+(?:\.[0-9]{1,4})?$/', 'min:1', 'max:15'),
