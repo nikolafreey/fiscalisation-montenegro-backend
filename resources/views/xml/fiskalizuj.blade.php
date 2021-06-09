a9835b15f32e86da09084fc1427791584a2b78cc
Miodrag Nikac <miodrag@restartit.me>
2021-06-09 07:29:32 +0000

    Fiskalizacija blade TotPrice ako nije pdv obveznik

--- resources/views/xml/fiskalizuj.blade.php
+++ resources/views/xml/fiskalizuj.blade.php
@@ -39,6 +39,7 @@
             TCRCode="{{ $taxpayer['CR'] }}"
             IsIssuerInVAT="{{ $pdv_obveznik }}"
             TotPriceWoVAT="{{ sprintf('%0.2f', $racun->ukupna_cijena_bez_pdv_popust) }}"
+            TotPrice="{{ sprintf('%0.2f', $racun->ukupna_cijena_sa_pdv_popust) }}"
             OperatorCode="{{ $taxpayer['OP'] }}"
             BusinUnitCode="{{ $taxpayer['BU'] }}"
             SoftCode="{{ $taxpayer['SW'] }}"
